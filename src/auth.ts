53ada815434a02eb6754b33f97b8e55a04b181f9
Muhammad Umer <mumer119131@gmail.com>
2024-10-03 00:42:01 +0500

    changing session to client side

--- src/auth.ts
+++ src/auth.ts
@@ -3,11 +3,12 @@ import { PrismaAdapter } from '@auth/prisma-adapter';
 import prisma from "./lib/prisma";
 import Google from 'next-auth/providers/google';
 import GitHub from 'next-auth/providers/github';
+import { Adapter } from 'next-auth/adapters';
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
     theme: {
         logo: '/logo.png',
     },
-    adapter: PrismaAdapter(prisma),
+    adapter: PrismaAdapter(prisma) as Adapter,
     providers: [Google, GitHub],
 })
